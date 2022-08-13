5289c51a964141eeb4e1ce3d84373f2bdeeccb74
Vishnu <mailto.vishnu.as@gmail.com>
2022-08-13 23:53:02 +0530

    This is my first commit.

---
+++ src/main/java/com/sdata/App.java
@@ -0,0 +1,21 @@
+package com.sdata;
+
+import java.util.ResourceBundle;
+
+public class App {
+
+	public int UserLogin(String User, String Pwd)
+	{
+		
+	ResourceBundle rb=ResourceBundle.getBundle("config");
+	String Uname=rb.getString("username");
+	String Pawd=rb.getString("password");
+	
+	if(User.equals(Uname) && Pwd.equals(Pawd))
+	
+		return 1;
+	
+	else
+		return 0;
+	}
+}
