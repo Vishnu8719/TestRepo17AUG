9f2c754d65ab4ba310e4d6b5d6d423360bc6b055
Vishnu <mailto.vishnu.as@gmail.com>
2022-08-17 20:38:45 +0530

    First Commit on 17 AUG 2022.

--- src/test/java/com/sdata/AppTest.java
+++ src/test/java/com/sdata/AppTest.java
@@ -10,16 +10,18 @@ public class AppTest {
 	
 	public void testlogin1()
 	{
+		
 		App myapp=new App();
 		Assert.assertEquals(0,myapp.UserLogin("admin", "123"));
 	}
 	
 	@Test
+	
 	public void testlogin2()
 	{
+		
 		App myapp=new App();
 		Assert.assertEquals(1,myapp.UserLogin("admin", "admin123"));
 	}
-}
 
- 
+}
